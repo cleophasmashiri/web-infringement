112e2b42cf24b53cde79c689d586425796aefdf2
Cleophas Mashiri <cleophasmashiri@gmail.com>
2020-08-14 14:04:06 +0200

    Initial version of webinfringement generated by JHipster-6.9.1

---
+++ src/main/webapp/app/admin/health/health.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { WebinfringementSharedModule } from 'app/shared/shared.module';
+
+import { HealthComponent } from './health.component';
+import { HealthModalComponent } from './health-modal.component';
+
+import { healthRoute } from './health.route';
+
+@NgModule({
+  imports: [WebinfringementSharedModule, RouterModule.forChild([healthRoute])],
+  declarations: [HealthComponent, HealthModalComponent],
+  entryComponents: [HealthModalComponent],
+})
+export class HealthModule {}
